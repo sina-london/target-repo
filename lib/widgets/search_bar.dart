0fb5ad91a7b830a704e1a2023c719f5e517d7215
RohitNexa <237893760+RohitNexa@users.noreply.github.com>
2024-11-13 06:38:19 +0530

    Search bar theme fix

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -12,39 +12,36 @@ class Searchbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        FocusScope.of(context).unfocus(); // Unfocuses any focused widget
-      },
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-        decoration: BoxDecoration(
-          color: Theme.of(context).highlightColor,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Row(
-          children: [
-            const Icon(
-              Icons.search,
-              size: 30,
-            ),
-            const SizedBox(width: 10),
-            Expanded(
-              child: TextField(
-                keyboardType: TextInputType.text,
-                controller: controller,
-                onEditingComplete: () {
-                  FocusScope.of(context).unfocus(); // Unfocus after submitting
-                  onSearch();
-                },
-                decoration: const InputDecoration(
-                  hintText: "Search Anime...",
-                  border: InputBorder.none,
-                ),
+    ThemeData _themeContext = Theme.of(context);
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+      decoration: BoxDecoration(
+        color: _themeContext.highlightColor,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Row(
+        children: [
+          const Icon(
+            Icons.search,
+            size: 30,
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: TextField(
+              keyboardType: TextInputType.text,
+              controller: controller,
+              onEditingComplete: () {
+                FocusScope.of(context).unfocus(); // Unfocus after submitting
+                onSearch();
+              },
+              style: _themeContext.textTheme.bodyMedium?.copyWith(fontSize: 18),
+              decoration: const InputDecoration(
+                hintText: "Search Anime...",
+                border: InputBorder.none,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
