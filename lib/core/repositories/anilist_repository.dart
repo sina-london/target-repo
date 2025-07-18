3f141f06d0733032749f9749100bd6e99df9cbf2
Ray-Lin92 <233557506+Ray-Lin92@users.noreply.github.com>
2025-07-19 01:30:24 +0530

    refactor: implement missing methods in AniListRepository

--- lib/core/repositories/anilist_repository.dart
+++ lib/core/repositories/anilist_repository.dart
@@ -3,66 +3,57 @@ import 'package:shonenx/core/models/anilist/anilist_media_list.dart';
 import 'package:shonenx/core/repositories/anime_repository.dart';
 
 class AniListRepository implements AnimeRepository {
-  final AnilistService  service;
+  final AnilistService service;
 
   AniListRepository(this.service);
 
   @override
-  Future<List<Media>> searchAnime(String title, {int page = 1, int perPage = 10}) {
+  Future<List<Media>> searchAnime(String title,
+      {int page = 1, int perPage = 10}) {
     return service.searchAnime(title, page: page, perPage: perPage);
   }
-  
+
   @override
   Future<Media> getAnimeDetails(int animeId) {
-    // TODO: implement getAnimeDetails
-    throw UnimplementedError();
+    return service.getAnimeDetails(animeId);
   }
-  
+
   @override
   Future<List<Media>> getFavorites() {
-    // TODO: implement getFavorites
-    throw UnimplementedError();
+    return service.getFavorites();
   }
-  
+
   @override
   Future<List<Media>> getPopularAnime() {
-    // TODO: implement getPopularAnime
-    throw UnimplementedError();
+    return service.getPopularAnime();
   }
-  
+
   @override
   Future<List<Media>> getRecentlyUpdatedAnime() {
-    // TODO: implement getRecentlyUpdatedAnime
-    throw UnimplementedError();
+    return service.getRecentlyUpdatedAnime();
   }
-  
+
   @override
   Future<List<Media>> getTopRatedAnime() {
-    // TODO: implement getTopRatedAnime
-    throw UnimplementedError();
+    return service.getTopRatedAnime();
   }
-  
+
   @override
   Future<List<Media>> getTrendingAnime() {
-    // TODO: implement getTrendingAnime
-    throw UnimplementedError();
+    return service.getTrendingAnime();
   }
-  
+
   @override
   Future<List<Media>> getUpcomingAnime() {
-    // TODO: implement getUpcomingAnime
-    throw UnimplementedError();
+    return service.getUpcomingAnime();
   }
-  
-  @override
-  Future<MediaListCollection> getUserAnimeList({required String type, required String status}) {
-    // TODO: implement getUserAnimeList
-    throw UnimplementedError();
-  }
-  
-  @override
-  // TODO: implement name
-  String get name => throw UnimplementedError();
 
-  
+  @override
+  Future<MediaListCollection> getUserAnimeList(
+      {required String type, required String status}) {
+    return service.getUserAnimeList(type: type, status: status);
+  }
+
+  @override
+  String get name => 'anilist';
 }
