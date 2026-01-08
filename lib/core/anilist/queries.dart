81417e58254d552fdf1c612fb53a92816b8bb0df
sina-london <182386088+sina-london@users.noreply.github.com>
2026-01-08 22:39:46 +0530

    feat: sort anime search by popularity descending

--- lib/core/anilist/queries.dart
+++ lib/core/anilist/queries.dart
@@ -190,7 +190,7 @@ class AnilistQueries {
   static const String searchAnimeQuery = '''
     query (\$search: String, \$page: Int, \$perPage: Int) {
       Page(page: \$page, perPage: \$perPage) {
-        media(search: \$search, type: ANIME) {
+        media(search: \$search, type: ANIME, sort: POPULARITY_DESC) {
           $mediaFields
         }
       }
