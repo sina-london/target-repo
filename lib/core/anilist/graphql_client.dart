35a9a365f5eb3a8e099e81f6ffdcab8857433971
sina-london <182386088+sina-london@users.noreply.github.com>
2026-01-08 22:32:53 +0530

    perf: improved caching for graphql queries.

--- lib/core/anilist/graphql_client.dart
+++ lib/core/anilist/graphql_client.dart
@@ -4,16 +4,20 @@ class AnilistClient {
   static final String _baseUrl = 'https://graphql.anilist.co';
 
   static GraphQLClient getClient({String? accessToken}) {
-    final HttpLink httpLink = HttpLink(
-      _baseUrl,
-      defaultHeaders: accessToken != null && accessToken.isNotEmpty
-          ? {'Authorization': 'Bearer $accessToken'}
-          : {},
-    );
+    final HttpLink httpLink = HttpLink(_baseUrl);
+    final AuthLink authLink = AuthLink(getToken: () async {
+      if (accessToken == null || accessToken.isEmpty) return null;
+      return 'Bearer $accessToken';
+    });
+    final Link link = authLink.concat(httpLink);
     return GraphQLClient(
-      link: httpLink,
-      cache: GraphQLCache(),
+      link: link,
+      cache: GraphQLCache(store: HiveStore()),
       queryRequestTimeout: Duration(seconds: 15),
+      defaultPolicies: DefaultPolicies(
+        query: Policies(fetch: FetchPolicy.cacheFirst),
+        mutate: Policies(fetch: FetchPolicy.networkOnly),
+      ),
     );
   }
 }
