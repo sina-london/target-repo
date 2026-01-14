2b74f85bb2c734713d0a5182a71fa45c98edba97
raulll-wow <225193462+raulll-wow@users.noreply.github.com>
2026-01-15 00:05:30 +0530

    fix(build-android): namespace missing.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,6 +12,19 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
+    afterEvaluate {
+        val project = this
+        if (project.hasProperty("android")) {
+            configure<com.android.build.gradle.BaseExtension> {
+                if (namespace == null) {
+                    namespace = project.group.toString()
+                }
+            }
+        }
+    }
+}
+
+subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
