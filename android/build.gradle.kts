68204c059124f9a25ee9e961346eabd9237803ad
sina-london <182386088+sina-london@users.noreply.github.com>
2026-01-15 00:39:22 +0530

    fix(build-android): lStar error

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,13 +12,31 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
+    project.layout.buildDirectory.set(buildDir.resolve(project.name))
+    
     afterEvaluate {
         val project = this
+        
         if (project.hasProperty("android")) {
             configure<com.android.build.gradle.BaseExtension> {
+                compileSdkVersion(35)
+                buildToolsVersion("35.0.0")
+
                 if (namespace == null) {
                     namespace = project.group.toString()
                 }
+
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+        }
+
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            compilerOptions {
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+                allWarningsAsErrors.set(false)
             }
         }
     }
